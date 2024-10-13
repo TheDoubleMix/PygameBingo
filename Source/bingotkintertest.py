3368c0560ae4781ca17d8fe01062555e9287f170
TheDoubleMix <xboxj2012@gmail.com>
2024-10-14 00:14:49 +0300

    add update to tkinter code

--- Source/bingotkintertest.py
+++ Source/bingotkintertest.py
@@ -8,8 +8,8 @@ import win32gui # type: ignore
 from threading import Thread
 global closed
 closed = False
-def open_popup():
-    global root, i, img
+def open_popup1():
+    global root, im, img
     top = tk.Toplevel(root)
     top.configure(background='white')
     top.iconphoto(False, img)
@@ -18,29 +18,41 @@ def open_popup():
     top.title("About")
     tk.Label(top, text= "Tehty Juho Jokisalo using python 3.12.7", font=('Segoe UI', 10), bg="white", fg="black").place(x=0,y=0)
     tk.Label(top, text= "Hello from the pygame community.", font=('Segoe UI', 10), bg="white", fg="black").place(x=0,y=20)
-    link = tk.Label(top, text= "https://www.pygame.org/contribute.html", font=('Segoe UI', 10), bg="white", fg="black")
-    link.place(x=207,y=20)
-    link.bind("<Enter>", lambda _:link.config(font=('Segoe UI', 10, "underline")))
-    link.bind("<Leave>", lambda _:link.config(font=('Segoe UI', 10)))
-    link.bind("<Button-1>", lambda _:os.system("start \"\" https://www.pygame.org/contribute.html"))
+    link1 = tk.Label(top, text= "https://www.pygame.org/contribute.html", font=('Segoe UI', 10), bg="white", fg="black")
+    link1.place(x=207,y=20)
+    link1.bind("<Enter>", lambda _:link1.config(font=('Segoe UI', 10, "underline")))
+    link1.bind("<Leave>", lambda _:link1.config(font=('Segoe UI', 10)))
+    link1.bind("<Button-1>", lambda _:os.system("start \"\" https://www.pygame.org/contribute.html"))
     tk.Label(top, text= "Minä aloitin luomaan tätä projektia Kuivasjärven Koululle 5/10/2024.", font=('Segoe UI', 10), bg="white", fg="black").place(x=0,y=40)
+    tk.Label(top, text= "Tämä ohjelma on avoimen lähdekoodin.", font=('Segoe UI', 10), bg="white", fg="black").place(x=0,y=60)
+    link2 = tk.Label(top, text= "https://github.com/TheDoubleMix/PygameBingo", font=('Segoe UI', 10), bg="white", fg="black")
+    link2.place(x=235,y=60)
+    link2.bind("<Enter>", lambda _:link2.config(font=('Segoe UI', 10, "underline")))
+    link2.bind("<Leave>", lambda _:link2.config(font=('Segoe UI', 10)))
+    link2.bind("<Button-1>", lambda _:os.system("start \"\" https://github.com/TheDoubleMix/PygameBingo"))
 def threaded_function(arg):
-    global closed, root, i, img
+    global closed, root, im, img, i
     root = tk.Tk()
     menu = tk.Menu(root)
     root.config(menu=menu)
     helpmenu = tk.Menu(menu, tearoff=False)
     menu.add_cascade(label='Help', menu=helpmenu)
     helpmenu.add_command(label='About', command=root.quit)
-    helpmenu.add_command(label='Debug', command=open_popup)
+    helpmenu.add_command(label='Debug', command=open_popup1)
     root.resizable(False, False)
     root.geometry('250x187')
     root.title("Console")
-    i = tk.PhotoImage(width=1, height=1)
+    im = tk.PhotoImage(width=1, height=1)
     img = tk.PhotoImage(data=pngfile)
     root.iconphoto(False, img)
-    tk.Button(root, text="Start", image=i, compound='c', height=85, width=250, font=('Segoe UI', 50)).pack()
-    tk.Button(root, text="Pause", image=i, compound='c', height=85, width=250, font=('Segoe UI', 50)).pack()
+    def reset():
+        start.config(text="Restart")
+        global i, text_width, text_line, reset_history, not_started
+        text_line, text_width, i, reset_history, not_started = 0, 0, 0, 1, False
+        pygame.display.flip()
+    start =tk.Button(root, text="Start", image=im, compound='c', height=85, width=250, font=('Segoe UI', 50), command=reset)
+    start.pack()
+    tk.Button(root, text="Pause", image=im, compound='c', height=85, width=250, font=('Segoe UI', 50)).pack()
     root.mainloop()
 numberhistoryrect = pygame.Rect(480, 0, 480, 480)
 currentletter = ""
@@ -58,7 +70,7 @@ pygame.display.set_caption('Bingo')
 GAME_WIDTH, GAME_HEIGHT = 960, 480
 bingonumbers = random.sample(range(1, 76), 75)
 font1 = pygame.font.Font(pygame.font.get_default_font(), 180)
-font2 = pygame.font.Font(pygame.font.get_default_font(), 48)
+font2 = pygame.font.Font(pygame.font.get_default_font(), 36)
 text_surface = font1.render(str(bingonumbers[0]), True, (255, 255, 255))
 class Rect:
     def __init__(self, size, position):
@@ -107,47 +119,56 @@ def init(screen):
 
 def draw(screen, game_objects):
     clock = pygame.time.Clock()
-    global closed
+    global closed, i, text_line, text_width, reset_history, not_started
+    reset_history = 0
     i = 0
     text_width = 0
     text_line = 0
+    not_started = True
     scroll_up = numberhistoryrect.move(0, -100)
+    print(bingonumbers)
+    while not_started:
+        screen.fill("black")
     while True:
         if closed == True:
             return
-        screen.fill("black", rect=(0,0,GAME_WIDTH/2,GAME_HEIGHT))
-        if  bingonumbers[i] < 15:
-            currentletter = "B"
-            pygame.draw.circle(screen,color1,(240,240),240)
-        elif  bingonumbers[i] < 30:
-            currentletter = "I"
-            pygame.draw.circle(screen,color2,(240,240),240)
-        elif  bingonumbers[i] < 45:
-            currentletter = "N"
-            pygame.draw.circle(screen,color3,(240,240),240)
-        elif  bingonumbers[i] < 60:
-            currentletter = "G"
-            pygame.draw.circle(screen,color4,(240,240),240)
-        else:
-            currentletter = "O"
-            pygame.draw.circle(screen,color5,(240,240),240)
-        text_surface = font1.render(currentletter + str(bingonumbers[i]), True, (255, 255, 255))
-        text_surface2 = font2.render((currentletter + str(bingonumbers[i])) + ", ", True, (255, 255, 255))
-        if 480 + text_width + text_surface2.get_width() > 960:
-            if not text_line + (text_surface2.get_height() * 2) > 480:
-                text_line += text_surface2.get_height()
+        if not i > 74:
+            screen.fill("black", rect=(0,0,GAME_WIDTH/2,GAME_HEIGHT))
+            if  bingonumbers[i] < 15:
+                currentletter = "B"
+                pygame.draw.circle(screen,color1,(240,240),240)
+            elif  bingonumbers[i] < 30:
+                currentletter = "I"
+                pygame.draw.circle(screen,color2,(240,240),240)
+            elif  bingonumbers[i] < 45:
+                currentletter = "N"
+                pygame.draw.circle(screen,color3,(240,240),240)
+            elif  bingonumbers[i] < 60:
+                currentletter = "G"
+                pygame.draw.circle(screen,color4,(240,240),240)
             else:
-                scroll_up = numberhistoryrect.move(0, text_surface2.get_height() * -1)
-                currentscreen = screen.subsurface(numberhistoryrect).copy()
+                currentletter = "O"
+                pygame.draw.circle(screen,color5,(240,240),240)
+            text_surface = font1.render(currentletter + str(bingonumbers[i]), True, (255, 255, 255))
+            text_surface2 = font2.render((currentletter + str(bingonumbers[i])) + ", ", True, (255, 255, 255))
+            if reset_history == 1:
                 screen.fill("black", rect=(480,0,GAME_WIDTH/2,GAME_HEIGHT))
-                screen.blit(currentscreen, scroll_up.topleft)
-            text_width = 0
-        screen.blit(text_surface2, (480+text_width, text_line))
-        text_width += text_surface2.get_width()
-        i += 1
-        screen.blit(text_surface, text_surface.get_rect(center=(240, 240)))
-        pygame.display.flip()
-        clock.tick(3)
+                reset_history = 0
+            if 480 + text_width + text_surface2.get_width() > 960:
+                if not text_line + (text_surface2.get_height() * 2) > 480:
+                    text_line += text_surface2.get_height()
+                else:
+                    scroll_up = numberhistoryrect.move(0, text_surface2.get_height() * -1)
+                    currentscreen = screen.subsurface(numberhistoryrect).copy()
+                    screen.fill("black", rect=(480,0,GAME_WIDTH/2,GAME_HEIGHT))
+                    screen.blit(currentscreen, scroll_up.topleft)
+                text_width = 0
+            screen.blit(text_surface2, (480+text_width, text_line))
+            text_width += text_surface2.get_width()
+            i += 1
+            screen.blit(text_surface, text_surface.get_rect(center=(240, 240)))
+            pygame.display.flip()
+            clock.tick(1)
 def run():
     if closed == False:
         hwnd = pygame.display.get_wm_info()['window']
